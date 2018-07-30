cd32141a4b5fa3c1f187cd7e5c60506b0aa1feeb
engbaher77 <eng.baher77@gmail.com>
2018-07-30 12:41:39 +0200

    Frist version

---
+++ 2.py
@@ -0,0 +1,31 @@
+import sys
+import paramiko
+import getpass
+
+def ssh_command(ssh):
+    #command = input("Command:")
+    command = "reboot"
+    ssh.invoke_shell()
+    stdin, stdout, stderr = ssh.exec_command(command)
+    print(stdout.read())
+
+def ssh_connect(host, user, key):
+    try:
+        ssh = paramiko.SSHClient()
+        print('Restarting ....')
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host, username=user, password=key)
+
+        ssh_command(ssh)
+    except Exception as e:
+        print('Connection Failed')
+        print(e)
+
+if __name__=='__main__':
+    #user = input("Username:")
+    user = "ubnt"
+    #key = input("Public key full path:")
+    key = "Mnf_city@P@ssw0rd"
+    host = input("Target Hostname:")
+    ssh_connect(host, user, key)
+
